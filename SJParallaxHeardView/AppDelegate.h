539b83a565bb462b156b3644174a3973b7c9fb5f
hsjcom <soldier@SoldierdeMacBook-Pro.local>
2015-05-08 15:12:15 +0800

    Initial Commit

---
+++ SJParallaxHeardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJParallaxHeardView
+//
+//  Created by Soldier on 15/5/8.
+//  Copyright (c) 2015年 Soldier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJParallaxHeardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJParallaxHeardView
+//
+//  Created by Soldier on 15/5/8.
+//  Copyright (c) 2015年 Soldier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
